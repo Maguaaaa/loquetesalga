14662fdb7b134ed6bdb28e7fd44e9ba2562f8566
Ricardo hoyos <riki.sagadeloshoyos@gmail.com>
2014-11-24 18:40:33 +0100

    comentarios version prealfa
    añadida la base de datos necesaria, las lineas para insertar el php
    comentarios en la pagina principal y un css provisional.

---
+++ bbdd comentarios/comentarios.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE `loquetesalga`.`comentarios` (
+`id` int( 100 ) NOT NULL AUTO_INCREMENT ,
+`nombre` varchar( 100 ) NOT NULL ,
+`email` varchar( 100 ) NOT NULL ,
+`comentario` varchar( 1000 ) NOT NULL ,
+`comentarioip` varchar( 100 ) NOT NULL ,
+`hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+`padre` int( 10 ) NOT NULL DEFAULT '0',
+`bien` int( 10 ) NOT NULL ,
+`bienip` varchar( 100 ) NOT NULL ,
+`mal` int( 10 ) NOT NULL ,
+`malip` varchar( 100 ) NOT NULL ,
+KEY `id` ( `id` )
+) ENGINE = MYISAM DEFAULT CHARSET = latin1;
--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -1,8 +1,7 @@
-<?php
+<link href="lugar/donde/esta/estilo.css" rel="stylesheet" type="text/css">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<?php
+$tabla='comentarios.sql';
+include 'comentarios.php';
+?>
 
